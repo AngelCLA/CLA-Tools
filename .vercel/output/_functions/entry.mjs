9d1ed0aec15bcad4edcc4f0116a0f4bcbaa66094
Luis Angel Carrillo <153340615+AngelCLA@users.noreply.github.com>
2026-02-02 15:43:27 -0700

    Remove prerender export from ebooks.astro

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CkUQl2qw.mjs';
-import { manifest } from './manifest__ReXvj1S.mjs';
+import { manifest } from './manifest_CfTxfzQo.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -31,7 +31,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "ab494555-5011-4be9-9b30-890ae2f8bfc6",
+    "middlewareSecret": "b23f5962-1355-4316-afdc-3a09c1c25a29",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);

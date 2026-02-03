b6206d863800776887c3c523e2f6c62e0ebe03fb
Luis Angel Carrillo <153340615+AngelCLA@users.noreply.github.com>
2026-02-02 20:06:06 -0700

    Update Astro configuration for static output and enable prerendering for ebooks page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   site: 'https://tools.claangel.site',
   integrations: [tailwind()],
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: false

de285d5e12c1968b49133f1170705619b1a8ad58
Luis Angel Carrillo <153340615+AngelCLA@users.noreply.github.com>
2026-02-02 17:28:10 -0700

    feat: update manifest file and middleware secret for improved routing functionality

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CkUQl2qw.mjs';
-import { manifest } from './manifest_C--OqbgG.mjs';
+import { manifest } from './manifest_DV8YNGSB.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -33,7 +33,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "2ec6c841-a353-43ed-8071-7d63734ea34b",
+    "middlewareSecret": "61ddec55-6b35-4623-80bb-c2d86670cb74",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
